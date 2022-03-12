0d53f3dfcc6d0fb9c6e4da8b8b0fdb193e075e87
Dzhambul Nurgalym <90133181+NurikJK@users.noreply.github.com>
2022-03-13 00:07:14 +0600

    Update 10.py

--- Lab5/10.py
+++ Lab5/10.py
@@ -3,5 +3,5 @@ import re
 text = 'CamelCaseLol'
 # output = camel_case_lol
 f = re.sub(r'(?!^)(?=[A-Z][a-z]+)', '_', text).lower()
-# f = re.sub(r'(?!^)(?=[A-Z][a-z]+[^A-Z]*)', '_', nur).lower()
+# f = re.sub(r'(?!^)(?=[A-Z][a-z]+[^A-Z]*)', '_', text).lower()
 print(f)
