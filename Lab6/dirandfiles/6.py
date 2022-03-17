a5df78adb81f054ea808340a4937cd7af1ec7b2d
Dzhambul Nurgalym <90133181+NurikJK@users.noreply.github.com>
2022-03-18 00:16:58 +0600

    Update 6.py

--- Lab6/dirandfiles/6.py
+++ Lab6/dirandfiles/6.py
@@ -7,16 +7,16 @@ if not os.path.exists(folder):
     os.mkdir(folder)
 
 dst = os.getcwd() + '\\' + folder
-# for i in range(len(alphabet)):
-#     if not os.path.exists(alphabet[i]):
-#         file = open(alphabet[i],'w')
-#         file.close()
-#         src = os.getcwd() + '\\' + str(alphabet[i])
-#         shutil.move(src, dst)
-
-for i in range(25):
-    if not os.path.exists(str(chr(i+65))):
-        file = open(strchr(i+65),'w')
+for i in range(len(alphabet)):
+    if not os.path.exists(alphabet[i]):
+        file = open(alphabet[i],'w')
         file.close()
-        src = os.getcwd() + '\\' + str(chr(i+65))
+        src = os.getcwd() + '\\' + str(alphabet[i])
         shutil.move(src, dst)
+
+# for i in range(25):
+#     if not os.path.exists(str(chr(i+65))):
+#         file = open(strchr(i+65),'w')
+#         file.close()
+#         src = os.getcwd() + '\\' + str(chr(i+65))
+#         shutil.move(src, dst)
