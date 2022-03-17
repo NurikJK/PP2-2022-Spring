a23ef855e6b77bae101f096bd5010f5b099fa596
Dzhambul Nurgalym <90133181+NurikJK@users.noreply.github.com>
2022-03-18 00:21:58 +0600

    Lab6

--- Lab6/dirandfiles/6.py
+++ Lab6/dirandfiles/6.py
@@ -11,12 +11,18 @@ for i in range(len(alphabet)):
     if not os.path.exists(alphabet[i]):
         file = open(alphabet[i],'w')
         file.close()
+        file = open(alphabet[i],mode = 'a')
+        file.write(alphabet[i])
+        file.close()
         src = os.getcwd() + '\\' + str(alphabet[i])
         shutil.move(src, dst)
 
 # for i in range(25):
 #     if not os.path.exists(str(chr(i+65))):
 #         file = open(strchr(i+65),'w')
+#         file.close()
+#         file = open(str(chr(i+65)))
+#         file.write(str(chr(i+65))
 #         file.close()
 #         src = os.getcwd() + '\\' + str(chr(i+65))
 #         shutil.move(src, dst)
